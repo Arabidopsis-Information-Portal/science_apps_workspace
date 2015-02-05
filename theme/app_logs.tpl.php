1cee23e85c3a0ac1222aa0d2f0cd495933ba9f63
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-02-05 10:23:47 -0600

    clearable app logs

--- theme/app_logs.tpl.php
+++ theme/app_logs.tpl.php
@@ -1,4 +1,4 @@
-<h2>Science App Logs</h2>
+<h2>Science App Logs <small><?php print $clear_logs_link ?></small></h2>
 
 <?php foreach ($logs as $log): ?>
 <small class="text-muted"><?php print date('c', $log['timestamp']) ?></small>
