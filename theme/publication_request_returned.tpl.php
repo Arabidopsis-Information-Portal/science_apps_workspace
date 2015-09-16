73f99edba72014aee9e3d1a5a94a4effaf393bef
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-09-16 11:50:11 -0500

    rendering request states

---
+++ theme/publication_request_returned.tpl.php
@@ -0,0 +1,11 @@
+<h2>More information required</h2>
+
+<p class="lead">
+  <?php echo $message ?>
+</p>
+
+<h4>Reviewer comments:</h4>
+<blockquote>
+  <?php echo $request->publication_notes ?>
+</blockquote>
+
